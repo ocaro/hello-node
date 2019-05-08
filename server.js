086e5719713f21739f065e08e39f9ce672b47f66
Caro, Orlando <46233280+chronominds@users.noreply.github.com>
2019-05-08 10:36:45 -0500

    npm test

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
+function sayHelloWorld() {
+  return 'Hello World!';
+}
+
+function testSayHelloWorld() {
+  var hello = sayHelloWorld();
+  if (hello == null || hello == '') {
+    console.log('Failed'.);
+  } else {
+    console.log('Passed.');
+  }
+}
+
 var http = require('http');
 
 var handleRequest = function(request, response) {
   console.log('Received request for URL: ' + request.url);
   response.writeHead(200);
-  response.end('Hello World!');
+  response.end(sayHelloWorld());
 };
+
 var www = http.createServer(handleRequest);
+
 www.listen(8080);
 
